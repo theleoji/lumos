a9927251f2c3207184190603ef249ef8f6ca30ca
Leo Ji <leo@leoji.dev>
2020-02-09 18:31:18 -0500

    analytics tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,8 +65,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-69406642-5`,
       },
     },
     {
