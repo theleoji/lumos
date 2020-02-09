ece78c110297ff5bf8c1d4bb6637a63b27c2cdc9
Leo Ji <leo@leoji.dev>
2020-02-09 17:40:25 -0500

    fix prop on 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,8 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout location={this.props.location}>
+const NotFoundPage = ({ location }) => (
+  <Layout location={location}>
     <SEO title="404: Not Found" />
     <h1>Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
